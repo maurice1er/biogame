02c7e333fe4b412c9d0e7b9f2ff9650494b116cf
maurice1er <armeldrey@gmail.com>
2023-06-02 16:19:46 +0000

    add models and swagger

--- models.py
+++ models.py
@@ -1,16 +1,14 @@
 from mongoengine import Document, StringField, ListField, IntField, DateTimeField, BooleanField, ReferenceField
 from datetime import datetime
-import json
 import uuid
-import random
 
 
 class Question(Document):
-    id = StringField(primary_key=True, default=str(uuid.uuid4()))
+    id = StringField(primary_key=True, default=lambda: str(uuid.uuid4()))
     question = StringField(required=True)
     options = ListField(StringField(), required=True)
     answer = StringField(required=True)
-    duration = IntField(default=7)
+    duration = IntField(default=5)
 
     created_by = StringField(required=True)
 
@@ -25,8 +23,7 @@ class Question(Document):
 
 
 class Participant(Document):
-    name = StringField(required=True)
-    score = IntField(default=0)
+    id = StringField(primary_key=True)
 
     meta = {
         'collection': 'participants'
@@ -34,8 +31,26 @@ class Participant(Document):
 
 
 class Challenge(Document):
-    id = StringField(primary_key=True)
-    challenger = ReferenceField('Participant')
-    challenged = ReferenceField('Participant')
-    winner = StringField(required=True)
-    challenge_date = DateTimeField(required=True)
+    id = StringField(primary_key=True, default=lambda: str(uuid.uuid4()))
+
+    challenger = ReferenceField(Participant)
+    challenged = ReferenceField(Participant, default=None)
+    winner = ReferenceField(Participant, default=None)
+
+    is_started = BooleanField(default=True)
+    is_ended = BooleanField(default=False)
+
+    launched_date = DateTimeField(default=datetime.now)
+
+    accepted_date = DateTimeField(default=None)
+    ended_date = DateTimeField(default=None)
+
+    # Score des participants
+    challenger_score = IntField(default=0)
+    challenged_score = IntField(default=0)
+
+    questions = ListField(ReferenceField(Question), required=True)
+
+    meta = {
+        'collection': 'challenges'
+    }
