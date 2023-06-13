07d1d9e8afb42fb4f34de0205c97f99cfa0a7ab5
maurice1er <armeldrey@gmail.com>
2023-06-13 02:58:15 +0000

    update backend

--- backend.py
+++ backend.py
@@ -452,6 +452,6 @@ def get_challenge(challenge_id: str):
 # # app.mount('/static', StaticFiles(directory='static'), name='static')
 if __name__ == '__main__':
     print("Backend Server started --> [::]:5000")
-    uvicorn.run("backend:app", host='localhost',
+    uvicorn.run("backend:app", host='0.0.0.0',
                 # log_level="info",
                 port=backend_port, workers=1, reload=True)
